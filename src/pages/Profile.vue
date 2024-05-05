84f851581cc496223996877d0f1bfeb4644a8306
marharytak <margaretdk@tut.by>
2024-05-05 15:15:59 +0300

    rework Profile page

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import router from "../router";
-import Orders from "../components/Orders.vue";
-import ProfileInfo from "../components/ProfileInfo.vue";
+import router from "@/router";
+import Orders from "@/components/Orders.vue";
+import NewOrder from "@/components/NewOrder.vue";
+import ProfileInfo from "@/components/ProfileInfo.vue";
 
-const selectedItem = ref("orders");
+const selectedItem = ref("new-order");
 
-const isOrdersActive = computed(() => selectedItem.value === 'orders');
-const isProfileInfoActive = computed(() => selectedItem.value === 'edit');
+const isNewOrderActive = computed(() => selectedItem.value === "new-order");
+const isOrdersActive = computed(() => selectedItem.value === "orders");
+const isProfileInfoActive = computed(() => selectedItem.value === "edit");
 
 function navigateTo(path: string) {
   router.replace(path);
@@ -21,12 +23,12 @@ function updateSelection(value: string) {
 <template>
   <v-card>
     <v-layout>
-      <v-navigation-drawer>
+      <v-navigation-drawer color="blue-grey-darken-1">
         <v-list>
           <v-list-item
             prepend-icon="mdi-account-outline"
-            subtitle="mail@gmail.com"
-            title="Verified User"
+            subtitle="eliseenk0@gmail.com"
+            title="Елисеенко Илья"
           ></v-list-item>
         </v-list>
 
@@ -38,6 +40,13 @@ function updateSelection(value: string) {
             title="На главную"
             value="home"
             @click="navigateTo('/')"
+          />
+          <v-list-item
+            prepend-icon="mdi-plus"
+            title="Новая заявка"
+            value="new-order"
+            :active="isNewOrderActive"
+            @click="updateSelection('new-order')"
           />
           <v-list-item
             prepend-icon="mdi-folder"
@@ -53,14 +62,13 @@ function updateSelection(value: string) {
             :active="isProfileInfoActive"
             @click="updateSelection('edit')"
           />
-          <!-- <v-list-item prepend-icon="mdi-cog-outline" title="Настройки" value="starred"></v-list-item> -->
-          <!-- <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>, -->
         </v-list>
       </v-navigation-drawer>
 
       <v-main style="height: 100vh">
-      <Orders v-if="isOrdersActive"></Orders>
-      <ProfileInfo v-if="isProfileInfoActive"></ProfileInfo>
+        <Orders v-if="isOrdersActive"></Orders>
+        <NewOrder v-if="isNewOrderActive"></NewOrder>
+        <ProfileInfo v-if="isProfileInfoActive"></ProfileInfo>
       </v-main>
     </v-layout>
   </v-card>
