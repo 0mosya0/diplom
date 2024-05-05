d0e093bbaa014b766c8a2f5076a87a59a396d8b5
marharytak <margaretdk@tut.by>
2024-05-05 13:41:14 +0300

    rework NavBar with composition api

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,36 @@
-<script>
-export default {
-  data: () => ({ value: 1 }),
-};
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = ref(1);
 </script>
 
 <template>
   <v-layout class="overflow-visible mx-14 my-4" style="height: 54px">
     <v-bottom-navigation
-      class="rounded-lg"
-      :elevation="7"
       v-model="value"
+      :elevation="7"
+      class="rounded-lg"
       color="teal"
       grow
     >
       <v-btn to="/">
-        <v-icon>mdi-history</v-icon>
-
+        <v-icon>mdi-home-outline</v-icon>
         Главная
       </v-btn>
 
       <v-btn to="/profile">
         <v-icon>mdi-heart</v-icon>
-
         Личный кабинет
       </v-btn>
 
-      <v-btn>
-        <v-icon>mdi-map-marker</v-icon>
+      <v-btn to="/org-info">
+        <v-icon>mdi-domain</v-icon>
+        Организациям
+      </v-btn>
 
-        Nearby
+      <v-btn to="/info">
+        <v-icon>mdi-information-outline</v-icon>
+        О нас
       </v-btn>
     </v-bottom-navigation>
   </v-layout>
