13f1571c8d995f991a7785df8cd732461975f7fa
marharytak <margaretdk@tut.by>
2024-05-05 13:42:36 +0300

    rework Home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,164 +1,35 @@
-<script setup lang="ts">
-/*
-import router from '../router';
-
-function openLoginPage() {
-    router.push('/about')
-}
-*/
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <NavBar />
-    <v-container class="fill-height">
-      <v-responsive class="align-centerfill-height mx-auto" max-width="900">
-        <v-img class="mb-4" height="150" src="@/assets/logo.png" />
+    <v-img height="100vh" cover src="/src/assets/home.jpg">
+      <NavBar />
+      <v-container class="fill-height">
+        <v-responsive class="align-center fill-height mx-auto" max-width="900">
+          <v-img class="mb-4" height="150" src="@/assets/home-icon.svg" />
 
-        <div class="text-center">
-          <div class="text-body-2 font-weight-light mb-n1">
-            Добро пожаловать в систему
+          <div class="text-center">
+            <h1 class="mb-1 text-teal-lighten-4">
+              Добро пожаловать в сервис для онлайн записи и получения услуг в
+              Республике Беларусь
+            </h1>
+
+            <v-btn class="mx-2 my-4" color="teal-darken-1" to="login">
+              Вход
+            </v-btn>
+
+            <v-btn class="my-4" color="teal-darken-1" to="registration">
+              Регистрация
+            </v-btn>
           </div>
-
-          <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-          <v-btn class="mx-2 my-4" to="login">Вход</v-btn>
-          <v-btn class="my-4" to="registration">Регистрация</v-btn>
-        </div>
-
-        <div class="py-4" />
-
-        <!-- <v-row>
-          <v-col cols="12">
-            <v-card
-              class="py-4"
-              color="surface-variant"
-              image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-              prepend-icon="mdi-rocket-launch-outline"
-              rounded="lg"
-              variant="outlined"
-            >
-              <template #image>
-                <v-img position="top right" />
-              </template>
-  
-              <template #title>
-                <h2 class="text-h5 font-weight-bold">Get started</h2>
-              </template>
-  
-              <template #subtitle>
-                <div class="text-subtitle-1">
-                  Replace this page by removing <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>pages/index.vue</v-kbd>.
-                </div>
-              </template>
-  
-              <v-overlay
-                opacity=".12"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-  
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://vuetifyjs.com/"
-              prepend-icon="mdi-text-box-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Learn about all things Vuetify in our documentation."
-              target="_blank"
-              title="Documentation"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-  
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-              prepend-icon="mdi-star-circle-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Explore available framework Features."
-              target="_blank"
-              title="Features"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-  
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://vuetifyjs.com/components/all"
-              prepend-icon="mdi-widgets-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Discover components in the API Explorer."
-              target="_blank"
-              title="Components"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-  
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://discord.vuetifyjs.com"
-              prepend-icon="mdi-account-group-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Connect with Vuetify developers."
-              target="_blank"
-              title="Community"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-        </v-row> -->
-      </v-responsive>
-    </v-container>
+        </v-responsive>
+      </v-container>
+    </v-img>
   </div>
 </template>
+
+<style>
+h2 {
+  text-shadow: 1px 1px 2px rgb(20, 37, 16);
+}
+</style>
